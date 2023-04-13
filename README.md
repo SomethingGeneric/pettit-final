377dfd6b1e17ee67e37f68689af8a3f1b63d2ae4
Matt C <matthew.compton@mymail.champlain.edu>
2023-04-12 21:12:35 -0400

    improves formatting of results page
    Co-authored-by: T20A02 <T20A026@users.noreply.github.com>
    Co-authored-by: Dave <dthomsen116@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Final Project for CSI-260
 This repository contains the code for the final project for CSI-260, created by [Matt Compton](https://github.com/SomethingGeneric), [Abijah Buttendorf](https://github.com/T20A026), and [David Thomsen](https://github.com/dthomsen116). The project involves developing a facial recognition module in Python using [insert libraries and technologies used].
 
-# Installation
-To run the code, you will need to install the following libraries:
-
+# Setup
 * `pip install -r requirements.txt`
 
 # Usage
-[Insert instructions on how to use the facial recognition module]
+`python app.py`
 
--- app.py
+++ app.py
@@ -4,31 +4,88 @@ import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOAD_FOLDER = "static/uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/upload', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload():
-    file = request.files['file']
+    file = request.files["file"]
     filename = file.filename
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
     return redirect(f"/analyze/{filename}")
+
 
 @app.route("/analyze/<filename>")
 def show_info(filename):
-    if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+    if not os.path.exists(os.path.join(app.config["UPLOAD_FOLDER"], filename)):
         return "how did we get here?"
     else:
         try:
-            face_analysis = DeepFace.analyze(img_path=os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            print(face_analysis)
-            return render_template('result.html', filename=filename, person=face_analysis[0])
+            person = DeepFace.analyze(
+                img_path=os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            )[0]
+
+            age = person["age"]  # normal
+            main_emotion = person["dominant_emotion"]  # normal
+            main_gender = person["dominant_gender"]  # normal
+            main_race = person["dominant_race"]  # normal
+
+            emotions = person["emotion"]  # dict
+            sorted_emotions = {
+                k: v
+                for k, v in sorted(
+                    emotions.items(), key=lambda item: item[1], reverse=True
+                )
+            }
+            clean_emotions = {}
+            for k, v in sorted_emotions.items():
+                if "e-" not in str(v):
+                    clean_emotions[k] = f"{v:.2f}"
+
+            genders = person["gender"]  # dict
+            sorted_genders = {
+                k: v
+                for k, v in sorted(
+                    genders.items(), key=lambda item: item[1], reverse=True
+                )
+            }
+            likely_genders = {}
+            for k, v in sorted_genders.items():
+                likely_genders[k] = f"{v:.2f}"
+
+
+            races = person["race"]  # dict
+            sorted_races = {
+                k: v
+                for k, v in sorted(
+                    races.items(), key=lambda item: item[1], reverse=True
+                )
+            }
+            likely_races = {}
+            for k, v in sorted_races.items():
+                if float(str(v).replace("%", "")) > 1.0:
+                    likely_races[k] = f"{float(str(v).replace('%','')):.2f}"
+
+            return render_template(
+                "result.html",
+                filename=filename,
+                age=age,
+                main_em=main_emotion,
+                main_gender=main_gender,
+                main_race=main_race,
+                s_em=clean_emotions,
+                s_gender=likely_genders,
+                s_races=likely_races,
+            )
         except Exception as e:
             return "skill issue: " + str(e)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
